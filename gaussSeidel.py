2d166c2d85dc9afa905f85107ad37360d6128d9e
Meblendorius <69820416+Meblendorius@users.noreply.github.com>
2021-12-05 18:11:59 -0300

    Update gaussSeidel.py

--- gaussSeidel.py
+++ gaussSeidel.py
@@ -1,5 +1,5 @@
 # Método Iterativo de Gauss-Seidel para resolver Sistemas
-#
+#verificar onde o delta fica maluco
 # 
 from numpy import round_, sqrt, around
 
@@ -8,9 +8,9 @@ def maxValue(a, b, c, eq1, eq2, eq3):
   delta = sqrt(
     ((eq1 - a)**2) + ((eq2 - b)**2) + ((eq3 - c)**2)
   )
-  roundDelta = around(delta, prec+1)
+  roundDelta = round(delta,5)
   
-  print(roundDelta)
+  #print(roundDelta)
   
   return roundDelta
 
@@ -36,18 +36,16 @@ def functionThree(x, y):
   
   return zf
 
-while deltaK != 0:
-  eq1 = around(functionOne(y, z), prec)
-  eq2 = around(functionTwo(x, z), prec)
-  eq3 = around(functionThree(x, y), prec)
+while deltaK != 0.0000:
+  eq1 = round(functionOne(y, z),5)
+  eq2 = round((functionTwo(x, z)),5)
+  eq3 = round((functionThree(x, y)),5)
   
   deltaK = maxValue(x, y, z, eq1, eq2, eq3)
-  
+  print("{}|X={} | Y={} | Z={} | x1={} | y1={}| z1={} | delta={}".format(i, x, y, z, eq1, eq2, eq3, deltaK))
+
   x = eq1
   y = eq2
   z = eq3
-  
-  i = i+1
-  print("X={} | Y={} | Z={} | iterações(k)= {}".format(x, y, z, i))
+  i = i + 1
 
-print("Resultado: X={} | Y={} | Z={} | iterações(k)= {}".format(x, y, z, i))
